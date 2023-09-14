a83f4c30db4d48e5ed45911c535659e76db24355
Matheus Henrique <matheush.borges@hotmail.com>
2023-09-14 20:04:15 -0300

    Back-end Done

---
+++ sistema/config.php
@@ -0,0 +1,16 @@
+<?php
+
+$email_adm = strtolower('atendimento@nhinteligencia.com.br');
+
+$servidor = 'localhost';
+$usuario = 'root';
+$senha = '';
+$banco = 'nh_Inteligencia_empresarial'
+
+
+// $servidor = 'localhost';
+// $usuario = 'mhborg31_DouglasRoot';
+// $senha = 'Douglas312@';
+// $banco = 'mhborg31_NH_Inteligencia'
+
+?>
